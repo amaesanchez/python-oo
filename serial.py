ed5516a20fe2c8e84ba206578698a607a8ef16a3
Arlaine Sanchez <amaesanchez@gmail.com>
2022-11-29 18:06:18 -0500

    serial generator

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,23 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """ initializes parameters """
+
+        self.start = self.current = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} current={self.current}>"
+
+    def generate(self):
+        """ increments the current variable """
+
+        self.current += 1
+
+        return self.current - 1
+
+    def reset(self):
+        """ resets current variable to the initial start # """
+
+        self.current = self.start
