7e4b2831bbe35777353ebac82dee7a7a73c82efb
Arlaine Sanchez <amaesanchez@gmail.com>
2022-11-29 20:02:54 -0500

    finish wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -14,11 +14,25 @@ class WordFinder:
         """ Parses file from path given """
 
         file = open(self.path)
-        list_of_words = [line for line in file]
+        list_of_words = [line.strip() for line in file]
         print(f"{len(list_of_words)} words read")
+
         return list_of_words
 
     def random(self):
         """ Returns random word """
 
-        return choice(self.words)[:-1]
+        return choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """ Special Word Finder: creates list of words that are not
+    blank spaces or start with '#' """
+
+    def parse(self):
+        """ returns list of valid words """
+        # review why super().words cant be accessed
+        # print("super_words", super().words)
+
+        filtered_words = [word for word in super().parse() if
+            len(word) > 1 and word[0] != "#"]
+        return filtered_words
